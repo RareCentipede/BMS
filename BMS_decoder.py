718d7b0b940727d118bb30cb2cd15424781f5d92
Zhengyang Lu <stevenlu176@gmail.com>
2024-05-19 16:33:18 +0200

    Separated decoding and plotting, and fixed current and power plot range

--- BMS_decoder.py
+++ BMS_decoder.py
@@ -2,96 +2,74 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from os import listdir
+from os.path import isfile, join
 
 # Read serial data
-with open('data/serial_20240516_224436.txt', 'r') as file:
-    hex = file.read().replace(" ", "")
+data_path = 'data/to_process/'
+data_to_process = [data_path + f for f in listdir(data_path) if isfile(join(data_path, f))]
 
-new_line_pos = 0
-while new_line_pos != -1:
-  new_line_pos = hex.find("\n", new_line_pos)
-  time_stamp = hex[new_line_pos:new_line_pos+13]
-  hex = hex.replace(time_stamp, "")
+for raw_data in data_to_process:
+  with open(raw_data, 'r') as file:
+      hex = file.read().replace(" ", "")
 
-hex.replace("\n", "")
+  new_line_pos = 0
+  while new_line_pos != -1:
+    new_line_pos = hex.find("\n", new_line_pos)
+    time_stamp = hex[new_line_pos:new_line_pos+13]
+    hex = hex.replace(time_stamp, "")
 
-measurements = []
+  hex.replace("\n", "")
 
-# Separate the frames
-header_1 = hex.find("55AAEB90", 0)
-header_2 = hex.find("55AAEB90", header_1+8)
+  measurements = []
 
-while header_2 != -1:
-  measurements.append(hex[header_1:header_2])
-  header_1 = header_2
+  # Separate the frames
+  header_1 = hex.find("55AAEB90", 0)
   header_2 = hex.find("55AAEB90", header_1+8)
 
-start_time = int(measurements[0][492:492+4], 16)
+  while header_2 != -1:
+    measurements.append(hex[header_1:header_2])
+    header_1 = header_2
+    header_2 = hex.find("55AAEB90", header_1+8)
 
-res = []
-index = 0
-frame = 1
+  start_time = int(measurements[0][492:492+4], 16)
 
-results = pd.DataFrame(columns=['frame', 'cell number', 'voltage', 'resistance', 'current', 'power'])
+  res = []
+  index = 0
+  frame = 1
 
-# Parse the data
-for m in measurements:
-  voltages = bytes.fromhex(m[12:12+24])
-  voltages_parsed = []
-  for i in range(0, len(voltages), 2):
-    v = int.from_bytes(voltages[i:i+2], byteorder='little', signed=False) * 0.001
-    voltages_parsed.append(v)
+  results = pd.DataFrame(columns=['frame', 'cell number', 'voltage', 'resistance', 'current', 'power'])
 
-  resistances = bytes.fromhex(m[160:160+24])
-  resistances_parsed = []
-  for i in range(0, len(resistances), 2):
-    r = int.from_bytes(resistances[i:i+2], byteorder='little', signed=False) * 0.001
-    resistances_parsed.append(r)
-  res.append((voltages_parsed, resistances_parsed))
+  # Parse the data
+  for m in measurements:
+    voltages = bytes.fromhex(m[12:12+24])
+    voltages_parsed = []
+    for i in range(0, len(voltages), 2):
+      v = int.from_bytes(voltages[i:i+2], byteorder='little', signed=False) * 0.001
+      voltages_parsed.append(v)
 
-  bytes = bytes.fromhex(m[316:316+8])
-  current_parsed = int.from_bytes(bytes, byteorder='little', signed=True) * 0.001 
+    resistances = bytes.fromhex(m[160:160+24])
+    resistances_parsed = []
+    for i in range(0, len(resistances), 2):
+      r = int.from_bytes(resistances[i:i+2], byteorder='little', signed=False) * 0.001
+      resistances_parsed.append(r)
+    res.append((voltages_parsed, resistances_parsed))
 
-  average_voltage = np.mean(voltages_parsed)
-  power_parsed = average_voltage * current_parsed
+    bytes = bytes.fromhex(m[316:316+8])
+    current_parsed = np.abs(int.from_bytes(bytes, byteorder='little', signed=True) * 0.001) 
 
-  frame_counter = int(m[10:12], 16)
+    average_voltage = np.mean(voltages_parsed)
+    power_parsed = np.sum(voltages_parsed) * current_parsed
 
-  for i in range(len(voltages_parsed)):
-    results.loc[index] = [frame, f'cell_{i+1}', voltages_parsed[i], resistances_parsed[i], current_parsed, power_parsed]
+    frame_counter = int(m[10:12], 16)
+
+    for i in range(len(voltages_parsed)):
+      results.loc[index] = [frame, f'cell_{i+1}', voltages_parsed[i], resistances_parsed[i], current_parsed, power_parsed]
+      index += 1
+
+    results.loc[index] = [frame, 'cell_average', average_voltage, np.mean(resistances_parsed), current_parsed, power_parsed]
     index += 1
+    frame += 1
 
-  results.loc[index] = [frame, 'cell_average', average_voltage, np.mean(resistances_parsed), current_parsed, power_parsed]
-  index += 1
-  frame += 1
-
-print(results)
-
-# plotting
-
-# plot voltages
-f, axes = plt.subplots(4, 1, sharex=True)
-ax_volt = axes[0]
-ax_res = axes[1]
-ax_cur = axes[2]
-ax_power = axes[3]
-
-sns.lineplot(results, x='frame', y='voltage', hue='cell number', ax=ax_volt)
-ax_volt.set_title('Voltages')
-ax_volt.set_ylabel('Voltage (V)')
-
-# plot resistances
-sns.lineplot(results, x='frame', y='resistance', hue='cell number', ax=ax_res, legend=True)
-ax_res.set_title('Resistances')
-ax_res.set_ylabel('Resistance ($\Omega$)')
-
-# plot current
-sns.lineplot(results, x='frame', y='current', ax=ax_cur)
-ax_cur.set_title('Current')
-ax_cur.set_ylabel('Current (A)')
-
-sns.lineplot(results, x='frame', y='power', ax=ax_power)
-ax_power.set_title('Power')
-ax_power.set_ylabel('Power (W)')
-
-plt.show()
+  data_name = raw_data.replace(data_path, "").replace(".txt", "").replace("results_", "")
+  results.to_csv(f'./Results/processed/results_{data_name}.csv')
---
+++ BMS_plotter.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+data_path = 'Results/to_plot/'
+data_to_plot = [data_path + f for f in listdir(data_path) if isfile(join(data_path, f))][0]
+
+results = pd.read_csv(data_to_plot)
+
+# plot voltages
+f, axes = plt.subplots(3, 1, sharex=True)
+ax_volt = axes[0]
+# ax_res = axes[1]
+ax_cur = axes[1]
+ax_power = axes[2]
+
+sns.lineplot(results, x='frame', y='voltage', hue='cell number', ax=ax_volt)
+ax_volt.set_title('Voltages')
+ax_volt.set_ylabel('Voltage (V)')
+
+# # plot resistances
+# sns.lineplot(results, x='frame', y='resistance', hue='cell number', ax=ax_res, legend=True)
+# ax_res.set_title('Resistances')
+# ax_res.set_ylabel('Resistance ($\Omega$)')
+
+# plot current
+sns.lineplot(results, x='frame', y='current', ax=ax_cur)
+ax_cur.set_ylim([results['current'][0], np.max(results['current'])])
+ax_cur.set_title('Current')
+ax_cur.set_ylabel('Current (A)')
+
+sns.lineplot(results, x='frame', y='power', ax=ax_power)
+ax_power.set_ylim([results['power'][0], np.max(results['power'])])
+ax_power.set_title('Power')
+ax_power.set_ylabel('Power (W)')
+
+plot_name = data_to_plot.replace(data_path, "").replace("results_", "").replace(".csv", "")
+f.savefig(f'Results/plots/plot_{plot_name}.png')
+plt.show()
